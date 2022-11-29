ba876404ff2334f6ab234a11d698f1e0110d7181
temka-ru <102544459+temka-ru@users.noreply.github.com>
2022-11-29 08:51:59 +0700

    v7.1

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
-from config import Configuration
+# from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_migrate import Migrate
+
+# app = Flask(__name__)
+# app.config.from_object(Configuration)
+
+# db = SQLAlchemy(app)
+
+# migrate = Migrate(app, db)
+
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
+
+
 app = Flask(__name__)
-app.config.from_object(Configuration)
+# app.config.from_object(Configuration)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
--- posts/blueprint.py
+++ posts/blueprint.py
@@ -8,5 +8,12 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def index():
-    return render_template('posts/index.html')
+    posts = Post.query.all()
+    return render_template('posts/index.html', posts=posts)
+
+# http://localhost/blog/first-post
+@posts.route('/<slug>')
+def post_detail(slug):
+    post = Post.query.filter(Post.slug==slug).first()
+    return render_template('posts/post_detail.html', post=post)
 
